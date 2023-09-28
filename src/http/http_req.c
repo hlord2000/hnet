6074b72a4499db1bf60e9085bc7db0523247afb1
Kelly Helmut Lord <helmut@helmutlord.com>
2023-09-28 00:26:20 -0400

    Successfully parses HTTP requests.

--- src/http/http_req.c
+++ src/http/http_req.c
@@ -1,163 +1,138 @@
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include <http.h>
 #include <http_req.h>
 
-static int validate_request_method(char *data_received, size_t data_length) {
-    if (data_received == NULL) {
+static http_req_method_t validate_method(char *method, size_t data_length) {
+
+    http_req_method_t http_method = NONE;
+    if (method == NULL) {
+        return NONE;
+    }
+
+    if (data_length == 0) {
+        return NONE;
+    }
+
+    if (strncmp(method, "GET", 3) == 0) {
+        http_method = GET;
+    } else if (strncmp(method, "POST", 4) == 0) {
+        http_method = POST;
+    } else if (strncmp(method, "PUT", 3) == 0) {
+        http_method = PUT;
+    } else if (strncmp(method, "DELETE", 6) == 0) {
+        http_method = DELETE;
+    } else {
+        http_method = NONE;
+    }
+}
+
+static int validate_path(char *path, size_t data_length) {
+    if (path == NULL) {
         return -1;
     }
 
-    if (data_length == NULL) {
-        return -1;
-    }
-
-};
-
-static int validate_request_path(char *data_received, size_t data_length) {
-    if (data_received == NULL) {
+    if (data_length == 0) {
         return -1;
     }
     
-    if (data_length == NULL) {
-        return -1;
-    }
-
-    if (strstr(data_received, "..") != NULL) {
-        return -1;
-    }
-    if (strstr(data_received, "#") != NULL) {
-        return -1;
-    }
-    if (strstr(data_received, "?") != NULL) {
-        return -1;
-    }
-    char *path[512]; 
+    char * path_exclusions[] = {"..", "//", "#", "?"};
     
-    strcpy(path, "./www", sizeof(path));
-    strncat(path, data_received, sizeof(path));
+    for (int i = 0; i < 4; i++) {
+        if (strstr(path, path_exclusions[i]) != NULL) {
+            return -1;
+        }
+    }
+    
+    char path_str[512];
+    strncpy(path_str, "./www", data_length);
+    strncat(path_str, path, data_length);
+    if (access(path_str, F_OK) == -1) {
+        return -1;
+    }
+    return 0;
+}
 
-    return 0; 
-};
+static int validate_version(char *http_version, size_t data_length) {
+    if (http_version == NULL) {
+        return -1;
+    } 
 
-static int validate_request_http_ver(char *data_received, size_t data_length) { if (data_received == NULL) {
+    if (data_length == 0) {
+        return -1;
+    }
+    
+    if (strncmp(http_version, "HTTP/1.1", 8) != 0) {
+        return -1;
+    }
+    return 0;
+}
+
+static int validate_request_line(char *request_line, size_t data_length, http_req_t *http_req) {
+    if (request_line == NULL) {
         return -1;
     }
 
-    if (data_length == NULL) {
+    if (data_length == 0) {
+        return -1;
+    }
+    
+    char *request_line_ptr;
+    char *method = strtok_r(request_line, " ", &request_line_ptr);
+    
+    if (method == NULL) {
         return -1;
     }
 
-};
+    if (validate_method(method, strlen(method)) == NONE) {
+        return -1;
+    }
+    
+    char *path = strtok_r(NULL, " ", &request_line_ptr);
 
-static int validate_request_start_line(char *data_received, size_t data_length) {
-    if (data_received == NULL) {
+    if (path == NULL) {
         return -1;
     }
 
-    if (data_length == NULL) {
+    if (validate_path(path, strlen(path))) {
         return -1;
     }
 
-    if (validate_request_method(data_received, data_length)) {
+    char *http_version = strtok_r(NULL, " ", &request_line_ptr);
+
+    if (http_version == NULL) {
         return -1;
     }
 
-    if (validate_request_path(data_received, data_length)) {
-        return -1;
-    }
-
-    if (validate_request_http_ver(data_received, data_length)) {
+    if (validate_version(http_version, strlen(http_version))) {
         return -1;
     }
 
     return 0;
-};
+}
 
-static int validate_request_header(char *data_received, size_t data_length) {
-    if (data_received == NULL) {
-        return -1;
-    }
-
-    if (data_length == NULL) {
-        return -1;
-    }
-    
-
-
-
-};
-
-static int validate_request_line(char *data_received, size_t data_length) {
-
-    if (data_received == NULL) {
-        return -1;
-    }
-
-    if (data_length == NULL) {
-        return -1;
-    }
-
-    char *start_line = strtok(data_received, "\r\n");
-    start_line = strtok(start_line, " ");
-
-    if (start_line == NULL) {
-        return -1;
-    }
-    
-    int request_line_index = 0; 
-
-    while (start_line != NULL) {
-        printf("start_line: %s\n", start_line);
-
-        switch (request_line_index) {
-            case 0:
-                if (strncmp(start_line, "GET", 3)) {
-                    break;
-                }
-                else if (strncmp(start_line, "POST", 4)) {
-                    break;
-                }
-                else {
-                    return -1;
-                }
-            case 1:
-                size_t path_length = strlen(start_line); 
-                if (validate_request_path(start_line, path_length)) {
-                    return -1;
-                }
-                break;
-            case 2:
-                if (!strncmp(start_line, "HTTP/1.1", 8)) {
-                    break;
-                }
-                else {
-                    return -1;
-                }
-            default:
-                return -1;
-        }
-        start_line = strtok(NULL, " ");
-
-        request_line_index++;
-    }
-};
-
-int validate_request(char *data_received, size_t data_length) {
+// Should either return a reference to an http_req_t or an error
+http_req_t * validate_request(char *data_received, size_t data_length) {
     
     int err;
     if (data_received == NULL) {
-        return -1;
+        return HTTP_REQ_ERROR;
     }
 
-    if (data_length == NULL) {
-        return -1;
+    if (data_length == 0) {
+        return HTTP_REQ_ERROR;
     }
+   
+    char *header_ptr;
+    char *request_line = strtok_r(data_received, "\r\n\r\n", &header_ptr);
     
-    err = validate_request_line(data_received, data_length);
-    if (err) {
-        return -1;
-    }
+    http_req_t *http_req;
 
+    err = validate_request_line(data_received, data_length, http_req);
+    if (err) {
+        return HTTP_REQ_ERROR;
+    }
+    return ;
 };
--- src/inc/http_req.h
+++ src/inc/http_req.h
@@ -7,7 +7,12 @@
 typedef enum {
     GET,
     POST,
+    PUT,
+    DELETE,
+    NONE,
 } http_req_method_t;
+
+#define HTTP_REQ_ERROR -1
 
 typedef struct {
     http_req_method_t request_method;
@@ -15,8 +20,20 @@ typedef struct {
     server_path_t server_path;
 
     http_version_t http_version;
+    
+    char **headers;
+
+    char *body;
 } http_req_t;
 
-int validate_request(char *data_received, size_t data_length);
+typedef enum {
+    HTTP_OK = 200,
+    HTTP_BAD_REQUEST = 400,
+    HTTP_NOT_FOUND = 404,
+    HTTP_METHOD_NOT_ALLOWED = 405,
+    HTTP_INTERNAL_SERVER_ERROR = 500,
+} http_ret_code_t;
+
+http_req_t * validate_request(char *data_received, size_t data_length);
 
 #endif
